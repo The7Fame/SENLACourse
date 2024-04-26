854978c19f7b4bc47e6cd8a1ec7379e050cc4951
DD <the7fame@gmail.com>
2024-04-26 17:39:14 +0300

    add dependencies and empty class

---
+++ src/main/java/eu/senla/naumovich/configuration/security/SecurityFilterChain.java
@@ -0,0 +1,6 @@
+package eu.senla.naumovich.configuration.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityFilterChain extends AbstractSecurityWebApplicationInitializer {
+}
