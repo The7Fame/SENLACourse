f4028ef2552fc8c54c91569e8c6fe3e59855057e
the7fame <the7fame@gmail.com>
2024-07-01 18:48:07 +0300

    minor fixes

--- src/test/java/eu/senla/naumovich/dao/common/BaseRepositoryTest.java
+++ src/test/java/eu/senla/naumovich/dao/common/BaseRepositoryTest.java
@@ -1,14 +1,14 @@
 package eu.senla.naumovich.dao.common;
 
-import eu.senla.naumovich.config.TestConfig;
+import eu.senla.naumovich.Application;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { TestConfig.class })
+@SpringBootTest(classes = Application.class)
 public class BaseRepositoryTest {
     public Pageable applyPage(){
         return PageRequest.of(1, 5);
