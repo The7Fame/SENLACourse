64ed3cc43ddd298526c5af486a46573ebf7b67c1
the7fame <the7fame@gmail.com>
2024-07-02 16:01:49 +0300

    8694y6188 add actuator

--- src/main/java/eu/senla/naumovich/configuration/SecurityConfig.java
+++ src/main/java/eu/senla/naumovich/configuration/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/sign-in").permitAll()
+                        .requestMatchers("/auth/sign-in", "/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
