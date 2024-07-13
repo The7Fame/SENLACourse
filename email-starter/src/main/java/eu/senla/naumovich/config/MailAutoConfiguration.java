858b33a7bc75703d49c003fb9046bfe3252d2e96
the7fame <the7fame@gmail.com>
2024-07-13 22:25:31 +0300

    8694y6189 modify starter

--- email-starter/src/main/java/eu/senla/naumovich/config/MailAutoConfiguration.java
+++ email-starter/src/main/java/eu/senla/naumovich/config/MailAutoConfiguration.java
@@ -1,40 +1,22 @@
 package eu.senla.naumovich.config;
 
 import eu.senla.naumovich.service.MailService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 @EnableConfigurationProperties(MailServiceProperties.class)
+@RequiredArgsConstructor
 public class MailAutoConfiguration {
-    @Autowired
-    private MailServiceProperties properties;
+    private final MailServiceProperties properties;
 
     @Bean
-    public JavaMailSender javaMailSender(){
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost(properties.getHostname());
-        javaMailSender.setUsername(properties.getUsername());
-        javaMailSender.setPassword(properties.getPassword());
-        javaMailSender.setPort(properties.getPort());
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", properties.getTsl());
-        props.put("mail.smtp.ssl.enable", properties.getSsl());
-        props.put("mail.debug", properties.getDebug());
-        javaMailSender.setJavaMailProperties(props);
-        return javaMailSender;
-    }
-
-    @Bean
+    @ConditionalOnClass(JavaMailSender.class)
     public MailService mailService(){
-        return new MailService(javaMailSender(), properties.getSenderAddress());
+        return new MailService(properties.getSenderAddress());
     }
 }
--- email-starter/src/main/java/eu/senla/naumovich/config/MailServiceProperties.java
+++ email-starter/src/main/java/eu/senla/naumovich/config/MailServiceProperties.java
@@ -10,15 +10,7 @@ import java.util.Map;
 
 @Setter
 @Getter
-@ConfigurationProperties(prefix = "spring.email")
+@ConfigurationProperties(prefix = "spring.mail")
 public class MailServiceProperties {
-    private String hostname = "smtp.yandex.ru";
-    private Integer port = 465;
-    private String username;
-    private String password;
-    private String protocol = "smtp";
-    private String senderAddress = "no-reply@mail.com";
-    private String ssl = "false";
-    private String tsl = "false";
-    private String debug = "true";
+    private String senderAddress;
 }
