f4779ad3a88937bfa7cca9eaa8063292520f7fc2
Maksim Naumovich <the7fame@gmail.com>
2025-03-10 12:54:05 +0300

    add new dto with payment and userinfo

--- bookstore-app/src/main/java/eu/senla/naumovich/dto/order/OrderWithPaymentDto.java
+++ bookstore-app/src/main/java/eu/senla/naumovich/dto/order/OrderWithPaymentDto.java
@@ -1,9 +1,7 @@
 package eu.senla.naumovich.dto.order;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonView;
-import eu.senla.naumovich.dto.order.view.View;
+import eu.senla.naumovich.dto.payment.PaymentDto;
 import eu.senla.naumovich.dto.payment.PaymentForOrderDto;
 import eu.senla.naumovich.dto.user.UserShortDto;
 import lombok.Builder;
@@ -18,11 +16,11 @@ import java.util.Date;
 @Setter
 @Builder
 @Jacksonized
-public class OrderShortDto {
+public class OrderWithPaymentDto {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
     private Date orderDate;
     private BigDecimal totalPrice;
-    private UserShortDto user;
+    private UserShortDto userInfo;
+    private PaymentForOrderDto payment;
 }
-
--- bookstore-app/src/main/java/eu/senla/naumovich/mapper/OrderMapper.java
+++ bookstore-app/src/main/java/eu/senla/naumovich/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package eu.senla.naumovich.mapper;
 
 import eu.senla.naumovich.dto.order.OrderDto;
 import eu.senla.naumovich.dto.order.OrderShortDto;
+import eu.senla.naumovich.dto.order.OrderWithPaymentDto;
 import eu.senla.naumovich.entities.Order;
 import eu.senla.naumovich.mapper.common.InterfaceMapper;
 import org.mapstruct.Mapper;
@@ -9,10 +10,14 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface OrderMapper extends InterfaceMapper<Order, OrderDto, OrderShortDto> {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
     @Mapping(source = "user", target = "userInfo")
-    OrderShortDto toOrderShortDto(Order order);
+    OrderWithPaymentDto toOrderWithPaymentDto(Order order);
+
+    List<OrderWithPaymentDto> toOrderWithPaymentDtoList(List<Order> orders);
 }
